2411daf446680727bbdf7987d904c0ea166a3987
Anderson <anderson.souzax@hotmail.com>
2017-10-10 20:24:44 -0300

    third commit

--- time-live/src/Local.js
+++ time-live/src/Local.js
@@ -14,8 +14,19 @@ class Local extends Component {
       token = sub.substring( sub.indexOf('=') + 1);
     }
 
+    //Objeto global
     let L = window.L;
-    // let map = L.map('map').setView([51.505, -0.09], 13);
+    
+    let map = L.map('map').setView([51.505, -0.09], 12);
+
+    //Exemplo pego da página oficial do leaflet
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+      maxZoom: 18,
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+        '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      id: 'mapbox.streets'
+    }).addTo(map);
 
     return (
       <div className="App">
@@ -23,7 +34,6 @@ class Local extends Component {
     {token}
         </header>
 
-          <div id="map"></div>
       </div>
     );
   }
